7ae906e1c47e5ada29240340a3f8b32c3d8fa18c
Ethan Szeto <ethan.szeto@gmail.com>
2024-04-08 11:37:53 -0400

    added styles

--- public/js/dev.js
+++ public/js/dev.js
@@ -1,10 +1,10 @@
 document.getElementById("query-button").addEventListener("click", query);
 window.addEventListener("keypress", (e) => {
   if (e.key == "`") {
-    if (document.getElementById("dev").style.display == "none") {
-      document.getElementById("dev").style.display = "block";
-    } else {
+    if (document.getElementById("dev").style.display == "block") {
       document.getElementById("dev").style.display = "none";
+    } else {
+      document.getElementById("dev").style.display = "block";
     }
   }
 });
