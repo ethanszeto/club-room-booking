de727497c5b49cf94c90e053feef4e53c2a769a3
Ethan Szeto <ethan.szeto@gmail.com>
2024-04-17 16:54:11 -0400

    Create club validation and frontend redesigns

--- public/js/create_club.js
+++ public/js/create_club.js
@@ -2,5 +2,13 @@ document.getElementById("create-club").addEventListener("click", createClub);
 
 async function createClub() {
   clubName = document.getElementById("club-name").value;
-  await request("POST", { club_name: clubName }, "/club/create");
+  let response = await request("POST", { club_name: clubName }, "/club/create");
+  if (response.error) {
+    console.log(response.error);
+    alert("Something went wrong. Try a different club name.");
+    window.location.href = "/";
+  } else {
+    document.getElementById("create-club-message").innerHTML = "Successfully created club!";
+    document.getElementById("club-name").value = "";
+  }
 }
