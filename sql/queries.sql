a07368aa4d3bb3759ce0869b42d0666d4935d252
Ethan Szeto <ethan.szeto@gmail.com>
2024-04-17 21:02:42 -0400

    Create queries.sql

---
+++ sql/queries.sql
@@ -0,0 +1,11 @@
+USE club_meetings;
+
+SELECT * FROM user;
+SELECT * FROM club;
+SELECT * FROM team;
+SELECT * FROM team_to_user;
+
+-- --------------------- --
+
+SELECT * FROM meeting;
+SELECT * FROM meeting_group;
